7f8b80dd26b1b18be41299bccf5093d8744f623e
Arran Ubels <a.ubels@base2services.com>
2018-07-13 12:42:39 +1000

    Change not complete

--- bin/ciinabox-ecs.rb
+++ bin/ciinabox-ecs.rb
@@ -38,7 +38,7 @@ class CiinaboxEcsCli
     methods = margs[0..margs.size()-2]
 
     unless ENV.key? 'CIINABOX'
-      ciinabox_name = args[args.size()-1]
+      ciinabox_name = margs[margs.size()-1]
       ENV['CIINABOX'] = ciinabox_name
     end
 
